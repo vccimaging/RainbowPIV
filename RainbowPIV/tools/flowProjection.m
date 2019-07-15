aafe7c6a3688da7294a74b9b745ae4055eae24e1
Jinhui Xiong <6891351+jhxiong@users.noreply.github.com>
2019-07-15 19:38:24 +0300

    change the liner solver

--- RainbowPIV/tools/flowProjection.m
+++ RainbowPIV/tools/flowProjection.m
@@ -1,7 +1,7 @@
 function uv = flowProjection( uv_tmp, dx, dy, dz, Rp, Rpt )
-
+     
      div = dx'*uv_tmp(:,1) + dy'*uv_tmp(:,2) + dz'*uv_tmp(:,3);     
-     P = Rp\(Rpt\div);
+     P = pcg(L, div, 1e-6, 100, Rp, Rpt);
      uv_tmp(:,1) = uv_tmp(:,1) - dx*P;
      uv_tmp(:,2) = uv_tmp(:,2) - dy*P;
      uv_tmp(:,3) = uv_tmp(:,3) - dz*P;
